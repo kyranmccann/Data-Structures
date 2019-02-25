a5482426e80fcbaab94d1bb4d8de0f620dccd1a3
kyranmccann <kyran.mccann@gmail.com>
2019-02-25 15:41:49 -0800

    finish heap.py

--- heap/heap.py
+++ heap/heap.py
@@ -1,21 +1,41 @@
 class Heap:
-  def __init__(self):
-    self.storage = []
+    def __init__(self):
+        self.storage = []
 
-  def insert(self, value):
-    pass
+    def insert(self, value):
+        self.storage.append(value)
+        self._bubble_up(len(self.storage) - 1)
 
-  def delete(self):
-    pass
+    def delete(self):
+        return_item = self.storage[0]
+        self.storage[0] = self.storage[len(self.storage) - 1]
+        self.storage.pop()
+        self._sift_down(0)
+        return return_item
 
-  def get_max(self):
-    pass
+    def get_max(self):
+        return self.storage[0]
 
-  def get_size(self):
-    pass
+    def get_size(self):
+        return len(self.storage)
 
-  def _bubble_up(self, index):
-    pass
+    def _bubble_up(self, index):
+        while (index - 1) // 2 >= 0:
+            if self.storage[(index - 1) // 2] < self.storage[index]:
+                self.storage[index], self.storage[(index - 1) // 2] = self.storage[(index - 1) // 2], self.storage[index]
+            index = (index - 1) // 2
 
-  def _sift_down(self, index):
-    pass
+    def _sift_down(self, index):
+        while index * 2 + 1 <= len(self.storage) - 1:
+            if (index * 2) + 2 > len(self.storage) -1:
+                big = (index * 2) + 1
+            elif self.storage[(index * 2) + 1] > self.storage[(index * 2) + 2]:
+                big = (index * 2) + 1
+            else:
+                big = (index * 2) + 2
+
+            if self.storage[big] > self.storage[index]:
+                current = self.storage[big]
+                self.storage[big] = self.storage[index]
+                self.storage[index] = current
+            index = big
