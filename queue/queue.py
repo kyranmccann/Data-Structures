377984e20ccb817bf7831b1b1fdf159d4a09c867
kyranmccann <kyran.mccann@gmail.com>
2019-02-25 14:39:11 -0800

    update queue to use self.size (didn't notice it before)

--- queue/queue.py
+++ queue/queue.py
@@ -7,12 +7,14 @@ class Queue:
 
     def enqueue(self, item):
         self.storage.insert(0, item)
+        self.size += 1
 
     def dequeue(self):
         if self.len() != 0:
+            self.size -= 1
             return self.storage.pop()
         else:
             return None
 
     def len(self):
-        return len(self.storage)
+        return self.size
